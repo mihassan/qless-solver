246b04038344b2707f3d404e60a916fd9238d201
Md Imrul Hassan <mihassan@gmail.com>
2023-02-12 00:37:21 +1100

    Removed tiny dictionary and renamed small dictionary to words.

--- src/main/kotlin/controller/DictionaryLoader.kt
+++ src/main/kotlin/controller/DictionaryLoader.kt
@@ -5,8 +5,10 @@ import kotlinx.js.fetch
 import model.Dictionary
 
 object DictionaryLoader {
+  private const val DICTIONARY_PATH = "dictionary/words.txt"
+  
   suspend fun loadDictionary(): Dictionary {
-    val content = fetch("./dictionary/small.txt").text().await()
+    val content = fetch(DICTIONARY_PATH).text().await()
     return Dictionary.of(content)
   }
 }
