9f577c9122e967f79a991bf052176a75cd59b2ed
Md Imrul Hassan <mihassan@gmail.com>
2023-02-16 10:09:34 +1100

    Use material components for Grid and Paper for Cell.

--- src/main/kotlin/view/Gird.kt
+++ src/main/kotlin/view/Gird.kt
@@ -1,47 +1,81 @@
 package view
 
-import csstype.FontFamily
+import csstype.AlignItems
+import csstype.Display
 import csstype.FontSize
 import csstype.FontWeight
+import csstype.JustifyContent
 import csstype.NamedColor
+import csstype.PlaceItems
+import csstype.fr
 import csstype.px
-import emotion.react.css
+import csstype.rem
+import mui.material.Grid
+import mui.material.Paper
+import mui.system.sx
 import react.FC
 import react.Props
-import react.dom.html.ReactHTML.table
-import react.dom.html.ReactHTML.tbody
-import react.dom.html.ReactHTML.td
-import react.dom.html.ReactHTML.tr
 
 external interface GridProps : Props {
   var letters: List<List<String>>
 }
 
 val Grid = FC<GridProps> { props ->
-  table {
-    css {
-      paddingTop = 20.px
-      paddingBottom = 20.px
+  val rowCount = props.letters.size
+  val colCount = props.letters.firstOrNull()?.size ?: 0
+
+  Grid {
+    sx {
+      display = Display.inlineGrid
+      gridTemplateRows = csstype.repeat(colCount, 1.fr)
+      gridTemplateColumns = csstype.repeat(rowCount, 1.fr)
+      gap = 4.px
+      placeItems = PlaceItems.center
     }
-    tbody {
-      props.letters.forEach { row ->
-        tr {
-          row.forEach { letter ->
-            td {
-              css {
-                fontFamily = "Roboto Mono".unsafeCast<FontFamily>()
-                fontSize = FontSize.large
-                fontWeight = FontWeight.bolder
-                padding = 10.px
-                color = NamedColor.black
-                backgroundColor =
-                  if (letter.isNotBlank()) NamedColor.lightgray else NamedColor.darkgray
-              }
-              +letter
-            }
+    props.letters.forEach { row ->
+      row.forEach { letter ->
+        Paper {
+          sx {
+            display = Display.flex
+            width = 2.rem
+            height = 2.rem
+            fontSize = FontSize.large
+            fontWeight = FontWeight.bolder
+            alignItems = AlignItems.center
+            justifyContent = JustifyContent.center
+            borderRadius = 0.px
+            backgroundColor = if (letter.isNotBlank()) NamedColor.lightgray else NamedColor.darkgray
           }
+          elevation = 1
+          +letter
         }
       }
     }
   }
+  // table {
+  //   css {
+  //     paddingTop = 20.px
+  //     paddingBottom = 20.px
+  //   }
+  //   tbody {
+  //     props.letters.forEach { row ->
+  //       tr {
+  //         row.forEach { letter ->
+  //           td {
+  //             css {
+  //               fontFamily = "Roboto Mono".unsafeCast<FontFamily>()
+  //               fontSize = FontSize.large
+  //               fontWeight = FontWeight.bolder
+  //               padding = 10.px
+  //               color = NamedColor.black
+  //               backgroundColor =
+  //                 if (letter.isNotBlank()) NamedColor.lightgray else NamedColor.darkgray
+  //             }
+  //             +letter
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 }
