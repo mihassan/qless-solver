274322141454ea2fd23423e932753f951aa6938f
Md Imrul Hassan <mihassan@gmail.com>
2023-02-21 01:43:02 +1100

    Highlight words in the grid upon hovering on a cell.

--- src/main/kotlin/view/Gird.kt
+++ src/main/kotlin/view/Gird.kt
@@ -9,11 +9,13 @@ import csstype.NamedColor
 import csstype.fr
 import csstype.px
 import csstype.vmin
+import model.Point
 import mui.material.Grid
 import mui.material.Paper
 import mui.system.sx
 import react.FC
 import react.Props
+import react.useState
 
 external interface GridProps : Props {
   var letters: List<List<String>>
@@ -22,6 +24,7 @@ external interface GridProps : Props {
 val Grid = FC<GridProps> { props ->
   val rowCount = props.letters.size
   val colCount = props.letters.firstOrNull()?.size ?: 0
+  var highlightedCells by useState<Set<Point>>(emptySet())
 
   Grid {
     sx {
@@ -30,8 +33,9 @@ val Grid = FC<GridProps> { props ->
       gridTemplateRows = csstype.repeat(rowCount, 1.fr)
       gridTemplateColumns = csstype.repeat(colCount, 1.fr)
     }
-    props.letters.forEach { row ->
-      row.forEach { letter ->
+    props.letters.forEachIndexed { y, row ->
+      row.forEachIndexed { x, letter ->
+        val isHighlighted = Point(x, y) in highlightedCells
         Paper {
           sx {
             display = Display.flex
@@ -41,12 +45,41 @@ val Grid = FC<GridProps> { props ->
             fontWeight = FontWeight.bold
             alignItems = AlignItems.center
             justifyContent = JustifyContent.center
-            backgroundColor = if (letter.isNotBlank()) NamedColor.lightgray else NamedColor.darkgray
+            backgroundColor =
+              when {
+                letter.isBlank() -> NamedColor.lightgray
+                isHighlighted -> NamedColor.lightcyan
+                else -> NamedColor.white
+              }
           }
-          elevation = 1
+          elevation =
+            when {
+              letter.isBlank() -> 0
+              isHighlighted -> 12
+              else -> 3
+            }
           +letter
+          onMouseEnter = {
+            highlightedCells = buildSet {
+              (y downTo 0)
+                .takeWhile { props.letters[it][x].isNotBlank() }
+                .forEach { add(Point(x, it)) }
+              (y until rowCount)
+                .takeWhile { props.letters[it][x].isNotBlank() }
+                .forEach { add(Point(x, it)) }
+              (x downTo 0)
+                .takeWhile { props.letters[y][it].isNotBlank() }
+                .forEach { add(Point(it, y)) }
+              (x until colCount)
+                .takeWhile { props.letters[y][it].isNotBlank() }
+                .forEach { add(Point(it, y)) }
+            }
+          }
         }
       }
+    }
+    onMouseLeave = {
+      highlightedCells = emptySet()
     }
   }
 }
