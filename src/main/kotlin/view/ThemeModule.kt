d9f448d9f21413315580fe58b53912c9acaac94e
Md Imrul Hassan <mihassan@gmail.com>
2023-03-12 15:30:54 +1100

    Persist theme selection in local storage so that it does not reset on refreshing the page.

--- src/main/kotlin/view/ThemeModule.kt
+++ src/main/kotlin/view/ThemeModule.kt
@@ -1,5 +1,6 @@
 package view
 
+import kotlinx.browser.window
 import mui.material.CssBaseline
 import mui.material.styles.Theme
 import mui.material.styles.ThemeProvider
@@ -7,6 +8,8 @@ import react.FC
 import react.PropsWithChildren
 import react.StateInstance
 import react.createContext
+import react.useEffect
+import react.useEffectOnce
 import react.useState
 
 typealias ThemeState = StateInstance<Theme>
@@ -15,7 +18,21 @@ val ThemeContext = createContext<ThemeState>()
 
 val ThemeModule = FC<PropsWithChildren> { props ->
   val state = useState(Themes.Light)
-  val (theme) = state
+  val (theme, setTheme) = state
+
+  useEffectOnce {
+    window.localStorage.getItem("theme")?.let {
+      when (it) {
+        "light" -> setTheme(Themes.Light)
+        "dark" -> setTheme(Themes.Dark)
+        else -> {}
+      }
+    }
+  }
+
+  useEffect(state) {
+    window.localStorage.setItem("theme", theme.palette.mode.toString())
+  }
 
   ThemeContext(state) {
     ThemeProvider {
